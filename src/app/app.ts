17a66e621deb7441741b009bb08a3b9b0d89be33
Foddie2 <bensonagala@live.com>
2025-11-17 12:23:37 +0300

    Bug fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppointmentList } from './appointment-list/appointment-list';
 import { Navbar } from './navbar/navbar';
+import { Hero } from './hero/hero';
+import { Footer } from './footer/footer';
+import { Home } from './home/home';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppointmentList, Navbar],
+  imports: [RouterOutlet, AppointmentList, Navbar, Hero, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
