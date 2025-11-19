31f266ca9a24ae2205cf801601652c139e8895d2
Foddie2 <bensonagala@live.com>
2025-11-19 17:39:27 +0300

    Pages/components define

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
   templateUrl: './hero.html',
-  styleUrl: './hero.scss',
+  styleUrls: ['./hero.scss'],
 })
-export class Hero {}
+export class HeroComponent {
+  heroHeadline = 'Welcome to Our Amazing App!';
+  heroSubtext =
+    'Discover a world of possibilities with our innovative solutions.';
+  ctaText = 'Get Started Now';
+
+  onCtaClick(): void {
+    console.log('Call to action clicked!');
+    // Implement navigation or other logic here
+  }
+}
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from '../hero/hero';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-home',
   template: `
+    <app-hero></app-hero>
     <section>
       <form>
         <input type="text" placeholder="Filter by city" />
         <button class="bg-stone" type="button">Search</button>
       </form>
     </section>
+    <app-footer></app-footer>
   `,
   styleUrls: ['./home.scss'],
+  imports: [HeroComponent, Footer],
 })
 export class Home {}
