5e4f8fc567b068d344bc789dcf73ce4e3818b015
Foddie2 <bensonagala@live.com>
2025-12-12 13:04:37 +0300

    Bug fix

--- src/app/appointment-list/appointment-list.ts
+++ src/app/appointment-list/appointment-list.ts
@@ -10,8 +10,35 @@ import { Appointment } from '../models/appointment';
 export class AppointmentList {
   // Properties for new appointment inputs
   newAppointmentTitle: string = '';
-  newAppointmentDate: string = '';
+  newAppointmentDate: Date = new Date();
   newAppointmentDescription: string = '';
 
   appointments: Appointment[] = [];
+
+  // Method to add a new appointment
+  // addAppointment() {
+  //   alert(
+  //     this.newAppointmentTitle +
+  //       ' ' +
+  //       this.newAppointmentDate +
+  //       ' ' +
+  //       this.newAppointmentDescription
+  //   );
+
+  /*    if (this.newAppointmentTitle && this.newAppointmentDate) {
+      const newAppointment: Appointment = {
+        id: this.appointments.length + 1,
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate,
+        description: this.newAppointmentDescription,
+      };
+      this.appointments.push(newAppointment);
+    } */
 }
+
+// Clear input fields after adding
+/*   this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+      this.newAppointmentDescription = '';
+    }
+ */
